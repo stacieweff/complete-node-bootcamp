87a36228d1a4639db5dc5cfae2a355bd132fd975
weffelms <stacie.weffelmeyer@wwt.com>
2019-08-05 13:53:13 -0500

    streams practice

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -7,7 +7,7 @@ process.env.UV_THREADPOOL_SIZE = 3
 setTimeout(() => console.log('Timer 1 finished'), 0)
 setImmediate(() => console.log('Immediate 1 finished'))
 
-fs.readFile('/starter/test-file.txt', () => {
+fs.readFile('../starter/test-file.txt', () => {
   console.log('i/o finished')
 
   setTimeout(() => console.log('Timer 2 finished'), 0)
