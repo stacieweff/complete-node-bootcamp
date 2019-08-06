2ffd2308e4e871164e994389fcdd5f69ddf62695
weffelms <stacie.weffelmeyer@wwt.com>
2019-08-06 15:00:04 -0500

    create own middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,12 +4,22 @@ const express = require('express')
 const app = express()
 
 app.use(express.json()) //middleware
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 🤞')
+  next()
+})
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString()
+  next()
+})
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime)
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours
@@ -82,11 +92,14 @@ const deleteTour = (req, res) => {
   })
 }
 
-app.get('/api/v1/tours', getAllTours)
-app.get('/api/v1/tours/:id', getTour)
-app.post('/api/v1/tours', createTour)
-app.patch('/api/v1/tours/:id', updateTour)
-app.delete('/api/v1/tours/:id', deleteTour)
+// app.get('/api/v1/tours', getAllTours)
+// app.get('/api/v1/tours/:id', getTour)
+// app.post('/api/v1/tours', createTour)
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.delete('/api/v1/tours/:id', deleteTour)
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour)
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
 const port = 3000
 app.listen(port, () => {
