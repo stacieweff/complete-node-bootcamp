5e0a81b0d91d43ad3d2e0e799210f5655e262612
weffelms <stacie.weffelmeyer@wwt.com>
2019-08-07 14:06:53 -0500

    environment variables

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -6,7 +6,11 @@ const userRouter = require('./routes/userRoutes')
 const app = express()
 
 ///1) Middleware
-app.use(morgan('dev'))
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.use(express.json()) //middleware
 
 app.use(express.static(`${__dirname}/public`))
