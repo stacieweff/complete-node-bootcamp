96991f3ce11526817b2cda7ff4b99d32d133e1ee
weffelms <stacie.weffelmeyer@wwt.com>
2019-08-15 12:59:54 -0500

    connecting database, mongoose, creating a simple Tour model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,36 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-dotenv.config({path: './config.env'})
 const app = require('./app')
+dotenv.config({
+  path: './config.env'
+});
 
-const port = process.env.PORT || 3000
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => console.log('DB connection successful!'))
+
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+})
+
+const Tour = mongoose.model('Tour', toursSchema)
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
 })
