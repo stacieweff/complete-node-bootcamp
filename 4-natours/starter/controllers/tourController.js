479b43ccfa012856c34584d7f4ec24ac1e140547
weffelms <stacie.weffelmeyer@wwt.com>
2019-08-19 16:10:15 -0500

    another way of creating documents

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,17 +2,6 @@ const Tour = require('./../models/tourModel')
 
 // const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
 
-exports.checkBody = (req, res, next) => {
-  console.log('check body', req.body)
-  if (!req.body.name || !req.body.price) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Body - Missing Name or Price'
-    })
-  }
-  next()
-}
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime)
   res.status(200).json({
@@ -38,13 +27,25 @@ exports.getTour = (req, res) => {
   // })
 }
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour
-    // }
-  })
+exports.createTour = async (req, res) => {
+  // const newTour = new Tour({})
+  // newTour.save()
+
+  try {
+    const newTour = await Tour.create(req.body)
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid Data sent!'
+    })
+  }
 }
 
 exports.updateTour = (req, res) => {
