786229faf9c101a4074b8f13906a458e93172c16
weffelms <stacie.weffelmeyer@wwt.com>
2019-08-22 08:39:39 -0500

    reading documents

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,29 +2,41 @@ const Tour = require('./../models/tourModel')
 
 // const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime)
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours
-    // }
-  })
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find()
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error
+    })
+  }
+
 }
 
-exports.getTour = (req, res) => {
-  console.log(req.params)
-  const id = req.params.id * 1
-  // const tour = tours.find(el => el.id === id)
-
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour
-  //   }
-  // })
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id)
+    // Tour.findOne({ _id: req.params.id })
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error
+    })
+  }
 }
 
 exports.createTour = async (req, res) => {
