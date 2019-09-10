9828ba17e99169f4cfd6bb427198ebe3c77469ca
weffelms <stacie.weffelmeyer@wwt.com>
2019-09-10 11:25:24 -0500

    sorting

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -15,7 +15,17 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj)
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
     console.log(JSON.parse(queryStr))
-    const query = Tour.find(JSON.parse(queryStr))
+    let query = Tour.find(JSON.parse(queryStr))
+
+    //sorting
+    if(req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ')
+      console.log(sortBy)
+      query = query.sort(sortBy)
+      //sort('price ratingsAverage')
+    } else {
+      query = query.sort('-createdAt')
+    }
 
     //execute query
     const tours = await query
