7ff4a50afc22fb359dd56dcd41fd02b166b2a7a5
weffelms <stacie.weffelmeyer@wwt.com>
2019-09-10 11:41:52 -0500

    Limiting Fields

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -27,6 +27,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt')
     }
 
+    //field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ')
+      query = query.select(fields)
+    } else {
+      query = query.select('-__v')
+    }
+
     //execute query
     const tours = await query
 
