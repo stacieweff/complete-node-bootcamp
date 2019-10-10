ecb9126c33a68325ccf07119b6065c6aa2c9ea13
weffelms <stacie.weffelmeyer@wwt.com>
2019-10-10 16:12:49 -0500

    catching uncaught exceptions

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 SHUTTING DOWN...')
+  console.log(err.name, err.message)
+  process.exit(1)
+})
+
 dotenv.config({
   path: './config.env'
 });
@@ -20,8 +26,8 @@ const server = app.listen(port, () => {
 })
 
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message)
   console.log('UNHANDLED REJECTION! 💥 SHUTTING DOWN...')
+  console.log(err.name, err.message)
   server.close(() => {
     process.exit(1)
   })
