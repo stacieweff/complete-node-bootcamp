7e9eb6c1d263a522fe1133905f2b92bac089d456
weffelms <stacie.weffelmeyer@wwt.com>
2019-10-16 13:30:55 -0500

    modeling users

---
+++ 4-natours/starter/models/userModel.js
@@ -0,0 +1,32 @@
+cont mongoose = require('mongoose')
+const validator = require('validator')
+
+//name, email, photo, password, passwordConfirm
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password']
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = Tour
