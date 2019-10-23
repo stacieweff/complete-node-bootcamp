183f4d68dbf4a8c912a5e7eeae4e6b643e0b3255
weffelms <stacie.weffelmeyer@wwt.com>
2019-10-23 08:53:07 -0500

    Logging in Users

--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -5,6 +5,7 @@ const authController = require('./../controllers/authController')
 const router = express.Router()
 
 router.post('/signup', authController.signup)
+router.post('/login', authController.login)
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser)
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser)
