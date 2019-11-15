83110d15bafe139977b2720ddcd072f969f794ec
weffelms <stacie.weffelmeyer@wwt.com>
2019-11-15 15:20:53 -0600

    data sanitization

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
@@ -28,11 +30,18 @@ const limiter = rateLimit({
 })
 
 app.use('/api', limiter)
+app.use(xss())
 
 //Body parser, reading data from body into req.body
 app.use(express.json({
   limit: '10kb'
 })) //middleware
+
+// Data sanitization againse noSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitization against XSS - cross site scripting attacks
+app.use(xss())
 
 // Serving Static Files
 app.use(express.static(`${__dirname}/public`))
