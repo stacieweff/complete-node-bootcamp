649afe91cfadd0d896740ee70d21bb4b2d0d5056
weffelms <stacie.weffelmeyer@wwt.com>
2019-11-18 14:55:32 -0600

    modelling locations geospatial data

--- 4-natours/starter/dev-data/data/import-dev-data.js
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -14,7 +14,7 @@ mongoose.connect(DB, {
 }).then(() => console.log('DB connection successful!'))
 
 //READ JSON FILE
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'))
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
 
 //IMPORT data into DB
 const importData = async () => {
