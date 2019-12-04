a9370b3a4be979276f1e2de59941cbdfc833bb8a
weffelms <stacie.weffelmeyer@wwt.com>
2019-12-04 10:12:57 -0600

    adding missing authorization and authentication

--- 4-natours/starter/routes/reviewRoutes.js
+++ 4-natours/starter/routes/reviewRoutes.js
@@ -4,19 +4,17 @@ const authController = require('./../controllers/authController')
 
 const router = express.Router({ mergeParams: true })
 
-// POST /tour/1234/reviews
-// GET /tour/1234/reviews
-// POST /reviews
+router.use(authController.protect)
 
 router
 .route('/')
 .get(reviewController.getAllReviews)
-.post(authController.protect, authController.restrictTo('user'), reviewController.setTourUserIds, reviewController.createReview)
+.post(authController.restrictTo('user'), reviewController.setTourUserIds, reviewController.createReview)
 
 router
 .route('/:id')
 .get(reviewController.getReview)
-.patch(reviewController.updateReview)
-.delete(reviewController.deleteReview)
+.patch(authController.restrictTo('user', 'admin'), reviewController.updateReview)
+.delete(authController.restrictTo('user', 'admin'), reviewController.deleteReview)
 
 module.exports = router
--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -18,12 +18,20 @@ const router = express.Router()
 
 router.use('/:tourId/reviews', reviewRouter)
 
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+router.route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours)
 
-router.route('/tour-stats').get(tourController.getTourStats)
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
+router.route('/tour-stats')
+  .get(tourController.getTourStats)
+router.route('/monthly-plan/:year')
+  .get(authController.protect, authController.restrictTo('admin', 'lead-guide', 'guide'), tourController.getMonthlyPlan)
 
-router.route('/').get(authController.protect, tourController.getAllTours).post(tourController.createTour)
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour)
+router.route('/')
+  .get(tourController.getAllTours)
+  .post(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.createTour)
+router.route('/:id')
+  .get(tourController.getTour)
+  .patch(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.updateTour)
+  .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour)
 
 module.exports = router
