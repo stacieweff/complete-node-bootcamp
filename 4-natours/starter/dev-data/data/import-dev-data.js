5cdf4785564a7ae7a7db5ddc91feca49042feb91
weffelms <stacie.weffelmeyer@wwt.com>
2019-12-04 11:58:57 -0600

    importing review and user data

--- 4-natours/starter/dev-data/data/import-dev-data.js
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const Tour = require('./../../models/tourModel')
+const User = require('./../../models/userModel')
+const Review = require('./../../models/reviewModel')
 dotenv.config({
   path: './config.env'
 });
@@ -15,11 +17,15 @@ mongoose.connect(DB, {
 
 //READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'))
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'))
 
 //IMPORT data into DB
 const importData = async () => {
   try {
     await Tour.create(tours)
+    await User.create(users, { validateBeforeSave: false })
+    await Review.create(reviews)
     console.log('data successfully loaded!')
   } catch (error) {
     console.log(error)
@@ -30,7 +36,9 @@ const importData = async () => {
 //Delete all data from collection
 const deleteData = async () => {
   try {
+    await User.deleteMany()
     await Tour.deleteMany()
+    await Review.deleteMany()
     console.log('data successfully deleted!')
   } catch (error) {
     console.log(error)
