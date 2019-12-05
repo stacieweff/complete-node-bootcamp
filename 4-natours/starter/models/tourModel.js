9500d780fdb3f67a202f383adb3545a2aa3052ab
weffelms <stacie.weffelmeyer@wwt.com>
2019-12-05 09:12:22 -0600

    geospacial aggregation: calculating distances

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -179,11 +179,11 @@ toursSchema.post(/^find/, function(docs, next) {
 })
 
 // AGGREGATION MIDDLEWARE
-toursSchema.pre('aggregate', function(next) {
-  this.pipeline().unshift({$match: { secretTour: { $ne: true } } })
-  console.log(this.pipeline())
-  next()
-})
+// toursSchema.pre('aggregate', function(next) {
+//   this.pipeline().unshift({$match: { secretTour: { $ne: true } } })
+//   console.log(this.pipeline())
+//   next()
+// })
 
 const Tour = mongoose.model('Tour', toursSchema)
 
--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -28,6 +28,8 @@ router.route('/monthly-plan/:year')
 
 router.route('/tours-within/:distance/center/:latlng/unit/:unit').get(tourController.getToursWithin)
 
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances)
+
 router.route('/')
   .get(tourController.getAllTours)
   .post(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.createTour)
