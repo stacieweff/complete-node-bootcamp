1b82663748f5d09489162c74923d07c2708e8665
weffelms <stacie.weffelmeyer@wwt.com>
2019-12-09 14:21:30 -0600

    setting up PUG with express

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
@@ -12,8 +13,13 @@ const userRouter = require('./routes/userRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
 
 const app = express()
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 
 ///1) Global Middlewares
+// Serving Static Files
+// app.use(express.static(`${__dirname}/public`))
+app.use(express.static(path.join(__dirname, 'public')))
 
 //SET Security HTTP Headers
 app.use(helmet())
@@ -52,9 +58,6 @@ app.use(hpp({
   ]
 }))
 
-// Serving Static Files
-app.use(express.static(`${__dirname}/public`))
-
 // test middleware
 app.use((req, res, next) => {
   console.log('Hello from the middleware 🤞')
@@ -67,6 +70,10 @@ app.use((req, res, next) => {
 })
 
 // Routes
+app.get('/', (req, res) => {
+  res.status(200).render('base')
+})
+
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/reviews', reviewRouter)
