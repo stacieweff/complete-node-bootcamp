8ee87a72cf8ef291a5b617a7bbf7a978d7642879
weffelms <stacie.weffelmeyer@wwt.com>
2019-12-17 13:34:46 -0600

    logging in users with our API part 1

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -6,6 +6,8 @@ const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
 const hpp = require('hpp')
+const cookieParser = require('cookie-parser')
+
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const viewRouter = require('./routes/viewRoutes')
@@ -44,7 +46,11 @@ app.use(xss())
 //Body parser, reading data from body into req.body
 app.use(express.json({
   limit: '10kb'
-})) //middleware
+})) 
+
+app.use(cookieParser())
+
+//middleware
 
 // Data sanitization againse noSQL query injection
 app.use(mongoSanitize())
@@ -66,7 +72,7 @@ app.use((req, res, next) => {
 })
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
-  // console.log(req.headers)
+  console.log(req.cookies)
   next()
 })
 
