2b95080e5649c60b19fea953a2078a1db497d32a
weffelms <stacie.weffelmeyer@wwt.com>
2019-12-17 09:02:52 -0600

    building the login screen

--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -6,5 +6,8 @@ const router = express.Router()
 router.get('/', viewsController.getOverview)
 router.get('/tour/:slug', viewsController.getTour)
 
+// /login
+router.get('/login', viewsController.getLoginForm)
+
 module.exports = router
 
