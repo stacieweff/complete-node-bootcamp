3cf857fe471d0bce48877b8b4baab7ef304352c6
weffelms <stacie.weffelmeyer@wwt.com>
2019-12-18 10:54:54 -0600

    logging in users with our api part 2

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -101,6 +101,31 @@ exports.protect = catchAsync(async (req, res, next) => {
   next()
 })
 
+// only for rendered pages, no errors!
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  if (req.cookies.jwt) {
+    //1) Verify token
+    const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET)
+
+    //2) Check if user still exists
+    const currentUser = await User.findById(decoded.id)
+    if (!currentUser) {
+      return next()
+    }
+
+    //3) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next()
+    }
+
+    // There is a logged in user
+    res.locals.user = currentUser
+    return next()
+  }
+  next()
+})
+
+
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     // roles is an array ['admin', 'lead-guide'] role = 'user'
