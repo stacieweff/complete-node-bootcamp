3d503046d599a02dca065f7e3830ec9c02df3fc2
weffelms <stacie.weffelmeyer@wwt.com>
2019-12-28 15:32:28 -0600

    loggin in users with our API-part3

--- 4-natours/starter/public/js/login.js
+++ 4-natours/starter/public/js/login.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
+import axios from 'axios'
+import { showAlert } from './alerts'
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -12,20 +14,12 @@ const login = async (email, password) => {
     })
 
     if (res.data.status === 'success') {
-      alert('Logged In Successfully!')
+      showAlert('success', 'Logged In Successfully!')
       window.setTimeout(() => {
         location.assign('/')
       }, 1500)
     }
   } catch (error) {
-    alert(error.response.data.message)
+    showAlert('error', error.response.data.message)
   }
 }
-
-document.querySelector('.form').addEventListener('submit', e => {
-  e.preventDefault()
-  const email = document.getElementById('email').value
-  const password = document.getElementById('password').value
-
-  login(email, password)
-})
--- 4-natours/starter/public/js/mapbox.js
+++ 4-natours/starter/public/js/mapbox.js
@@ -1,46 +1,45 @@
 /* eslint-disable */
 
-const locations = JSON.parse(document.getElementById('map').dataset.locations)
-console.log(locations)
+export const displayMap = (locations) => {
+  mapboxgl.accessToken = 'pk.eyJ1Ijoid2VmZmVsbXMiLCJhIjoiY2s0OGx0cnBzMTQ5bDNkbXJ3YTNvOGxzcCJ9.KkbXHYzgJVdLU1q4SXgomw';
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoid2VmZmVsbXMiLCJhIjoiY2s0OGx0cnBzMTQ5bDNkbXJ3YTNvOGxzcCJ9.KkbXHYzgJVdLU1q4SXgomw';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/weffelms/ck48mmskc4y0s1cqwikbe4oqo',
+    scrollZoom: false
+    // center: [-118.132986, 34.055439],
+    // zoom: 10,
+    // interactive: false
+  });
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/weffelms/ck48mmskc4y0s1cqwikbe4oqo',
-  scrollZoom: false
-  // center: [-118.132986, 34.055439],
-  // zoom: 10,
-  // interactive: false
-});
+  const bounds = new mapboxgl.LngLatBounds()
 
-const bounds = new mapboxgl.LngLatBounds()
+  locations.forEach(loc => {
+    // create marker
+    const el = document.createElement('div')
+    el.className = 'marker'
 
-locations.forEach(loc => {
-  // create marker
-  const el = document.createElement('div')
-  el.className = 'marker'
+    // add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    }).setLngLat(loc.coordinates).addTo(map)
 
-  // add marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom'
-  }).setLngLat(loc.coordinates).addTo(map)
+    // add popup
+    new mapboxgl.Popup({
+      offset: 30
+    }).setLngLat(loc.coordinates).setHTML(`<p>Day ${loc.day} : ${loc.description}</p>`).addTo(map)
 
-  // add popup
-  new mapboxgl.Popup({
-    offset: 30
-  }).setLngLat(loc.coordinates).setHTML(`<p>Day ${loc.day} : ${loc.description}</p>`).addTo(map)
+    // extend map bounds to include current location
+    bounds.extend(loc.coordinates)
+  })
 
-  // extend map bounds to include current location
-  bounds.extend(loc.coordinates)
-})
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100
-  }
-})
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+    }
+  })
+}
