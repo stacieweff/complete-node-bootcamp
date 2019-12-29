6cf5e92088d6c97086a72ac4cf86fcbe9a9e1859
weffelms <stacie.weffelmeyer@wwt.com>
2019-12-28 22:51:07 -0600

    rendering error pages

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -1,10 +1,10 @@
 const Tour = require('../models/tourModel')
 const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
 
 exports.getOverview = catchAsync(async(req, res, next) => {
   //1) get tour data from the collection
   const tours = await Tour.find()
-
   
   //2)Build template
   //3)Render that template using tour data from 1
@@ -22,6 +22,10 @@ exports.getTour = catchAsync(async(req, res, next) => {
     path: 'reviews',
     fields: 'review rating user'
   })
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404))
+  }
 
   // 2) build template
 
