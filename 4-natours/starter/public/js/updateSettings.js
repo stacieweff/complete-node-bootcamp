fa90dfa7b2de024dac2174892416616c18076199
weffelms <stacie.weffelmeyer@wwt.com>
2019-12-29 22:34:22 -0600

    udpating user data with our API

---
+++ 4-natours/starter/public/js/updateSettings.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+import axios from 'axios'
+import { showAlert } from './alerts'
+
+export const updateData = async (name, email) => {
+  try {
+    const res = await axios({
+      method: 'PATCH',
+      url: 'http://localhost:3000/api/v1/users/updateMe',
+      data: {
+        name,
+        email
+      }
+    })
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Data updated successfully!')
+      window.setTimeout(() => {
+        location.assign('/')
+      }, 1500)
+    }
+
+  } catch (error) {
+    showAlert('error', error.response.data.message)
+  }
+}
