03a91112050cfc25e5d8baa33c742007ac03d3c0
weffelms <stacie.weffelmeyer@wwt.com>
2020-01-02 12:43:24 -0600

    image uploads using multer

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -32,6 +32,9 @@ exports.getMe = (req, res, next) => {
 }
 
 exports.updateMe = catchAsync(async (req, res, next) => {
+  console.log(req.file)
+  console.log(req.body)
+
   // 1) Create Error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(new AppError('This route is not for password updates, Please use /updateMyPassword', 400))
