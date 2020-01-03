050d7fce5d8df67e61d765239711512f47c0e482
weffelms <stacie.weffelmeyer@wwt.com>
2020-01-03 12:54:36 -0600

    resizing images

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -3,17 +3,20 @@ const catchAsync = require('./../utils/catchAsync')
 const AppError = require('./../utils/appError')
 const factory = require('./handleFactory')
 const multer = require('multer')
+const sharp = require('sharp') // resizing images
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/img/users')
-  },
-  filename: (req, file, cb) => {
-    //user-userid-timestamp.ext
-    const ext = file.mimetype.split('/')[1]
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`)
-  }
-})
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users')
+//   },
+//   filename: (req, file, cb) => {
+//     //user-userid-timestamp.ext
+//     const ext = file.mimetype.split('/')[1]
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`)
+//   }
+// })
+
+const multerStorage = multer.memoryStorage()
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -29,6 +32,16 @@ const upload = multer({
 })
 
 exports.uploadUserPhoto = upload.single('photo')
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next()
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`
+
+  sharp(req.file.buffer).resize(500, 500).toFormat('jpeg').jpeg({ quality: 90 }).toFile(`public/img/users/${req.file.filename}`)
+
+  next()
+}
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {}
