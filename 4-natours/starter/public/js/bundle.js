cfd325bf7f625136bb8e94c070d207009ab78e67
weffelms <stacie.weffelmeyer@wwt.com>
2020-01-06 21:54:27 -0600

    adding image uploads to forms

--- 4-natours/starter/public/js/bundle.js
+++ 4-natours/starter/public/js/bundle.js
@@ -8864,12 +8864,12 @@ if (loginForm) {
 if (logoutBtn) logoutBtn.addEventListener('click', _login.logout);
 if (userDataForm) userDataForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  (0, _updateSettings.updateSettings)({
-    name: name,
-    email: email
-  }, 'data');
+  var form = new FormData();
+  form.append('name', document.getElementById('name').value);
+  form.append('email', document.getElementById('email').value);
+  form.append('photo', document.getElementById('photo').files[0]);
+  console.log(form);
+  (0, _updateSettings.updateSettings)(form, 'data');
 });
 if (userPasswordForm) userPasswordForm.addEventListener('submit',
 /*#__PURE__*/
@@ -8940,7 +8940,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56227" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56268" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
