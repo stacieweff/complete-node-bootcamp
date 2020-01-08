31a37403e11d20b49c06c36c4de6b91fc71f5867
weffelms <stacie.weffelmeyer@wwt.com>
2020-01-07 21:28:04 -0600

    sending password reset emails

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -166,15 +166,15 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false })
 
   //3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`
-
-  const message = `Forgot your password? Submit a PATCH request with your new password and password confirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`
   try {
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`
     // await sendEmail({
     //   email: user.email,
     //   subject: 'Your Password reset token (valid for 10 min)',
     //   message
     // })
+
+    await new Email(user, resetURL).sendPasswordReset()
 
     res.status(200).json({
       status: 'success',
