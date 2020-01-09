a109fba278ea579788b318c0c637336dbdc63dee
weffelms <stacie.weffelmeyer@wwt.com>
2020-01-09 09:24:28 -0600

    processing payments on the front end

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -40,8 +40,8 @@ const createSendToken = (user, statusCode, res) => {
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body)
-  const url = `${req.protocol}://${req.get('host')}/me`
-  // const url = 'http://localhost:3000/me'
+  // const url = `${req.protocol}://${req.get('host')}/me`
+  const url = 'http://localhost:3000/me'
   console.log(url)
   await new Email(newUser, url).sendWelcome()
   createSendToken(newUser, 201, res)
--- 4-natours/starter/public/js/login.js
+++ 4-natours/starter/public/js/login.js
@@ -6,7 +6,7 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://localhost:3000/api/v1/users/login',
+      url: 'http://localhost:3000/api/v1/users/login', // http://127.0.0.1:3000/api/v1/users/login
       data: {
         email,
         password
