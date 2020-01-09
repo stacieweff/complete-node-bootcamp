2c6eea5f9ce145330fc1a15c3ef972dc016ae624
weffelms <stacie.weffelmeyer@wwt.com>
2020-01-09 09:40:54 -0600

    some fixes, adjustments

--- 4-natours/starter/controllers/bookingController.js
+++ 4-natours/starter/controllers/bookingController.js
@@ -13,8 +13,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
-    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    success_url: `http://localhost:3000/`, // `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `http://localhost:3000/tour/${tour.slug}`, // `${req.protocol}://${req.get('host')}/tour/${tour.slug}`, 
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
     line_items: [
@@ -22,7 +22,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         name: `${tour.name} Tour`,
         description: tour.summary,
         images: [
-          `https://www.natours.dev/img/tours/${tour.imageCover}`
+          `https://www.natours.dev/img/tours/${tour.imageCover}` // `http://localhost:3000/img/tours/${tour.imageCover}` // `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}` // `https://www.natours.dev/img/tours/${tour.imageCover}`
         ],
         amount: tour.price * 100,
         currency: 'usd',
